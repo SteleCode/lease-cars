5e7e0dfaa6033faf44265dfa23a6604646a6d528
SteleCode <mrzdmitro@gmail.com>
2025-05-03 20:17:11 +0300

    upload

--- js/contact.js
+++ js/contact.js
@@ -2,3 +2,10 @@ document.querySelector('.form').addEventListener('submit', function (e) {
     e.preventDefault(); // Зупиняємо стандартну відправку форми
     this.reset(); // Очищаємо всі поля форми
   });
+
+  const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', ()=>{
+  menu.classList.toggle('menu__list--active');
+});
